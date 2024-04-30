ff392f7bd7e395fa7f5053a71ba1fd95211c05f1
yuki designs <yukiosdesigns@gmail.com>
2024-04-30 14:00:00 +0300

    Made changes

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,8 @@
-import { Room } from "./Room";
-import { CollaborativeApp } from "./CollaborativeApp";
 
 export default function Page() {
-  return (
-    <Room>
-      <CollaborativeApp />
-    </Room>
+  return (   
+    <div className="h-[100vh] w-full flex justify-center items-center text-center">
+      <h1 className="text-2xl text-white">Live Yuki clone</h1>
+      </div>
   );
 }
