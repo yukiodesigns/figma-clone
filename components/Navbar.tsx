042f0971951e3c689d3535e3e76b9af6c6543c6f
yuki designs <yukiosdesigns@gmail.com>
2024-05-01 12:54:05 +0300

    Added components and other functionality updated

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,11 +1,8 @@
 "use client";
-
 import Image from "next/image";
 import { memo } from "react";
-
 import { navElements } from "@/constants";
 import { ActiveElement, NavbarProps } from "@/types/type";
-
 import { Button } from "./ui/button";
 import ShapesMenu from "./ShapesMenu";
 import ActiveUsers from "./users/ActiveUser";
@@ -22,45 +19,23 @@ const Navbar = ({ activeElement, imageInputRef, handleImageUpload, handleActiveE
 
       <ul className="flex flex-row">
         {navElements.map((item: ActiveElement | any) => (
-          <li
-            key={item.name}
-            onClick={() => {
+          <li key={item.name} onClick={() => {
               if (Array.isArray(item.value)) return;
               handleActiveElement(item);
-            }}
-            className={`group px-2.5 py-5 flex justify-center items-center
-            ${isActive(item.value) ? "bg-primary-green" : "hover:bg-primary-grey-200"}
-            `}
-          >
-            {/* If value is an array means it's a nav element with sub options i.e., dropdown */}
+            }} className={`group px-2.5 py-5 flex justify-center items-center ${isActive(item.value) ? "bg-primary-green" : "hover:bg-primary-grey-200"}`}  >
             {Array.isArray(item.value) ? (
-              <ShapesMenu
-                item={item}
-                activeElement={activeElement}
-                imageInputRef={imageInputRef}
-                handleActiveElement={handleActiveElement}
-                handleImageUpload={handleImageUpload}
-              />
+              <ShapesMenu item={item} activeElement={activeElement} imageInputRef={imageInputRef} 
+              handleActiveElement={handleActiveElement} handleImageUpload={handleImageUpload} />
             ) : item?.value === "comments" ? (
               // If value is comments, trigger the NewThread component
               <NewThread>
                 <Button className="relative w-5 h-5 object-contain">
-                  <Image
-                    src={item.icon}
-                    alt={item.name}
-                    fill
-                    className={isActive(item.value) ? "invert" : ""}
-                  />
+                  <Image src={item.icon} alt={item.name} fill className={isActive(item.value) ? "invert" : ""}/>
                 </Button>
               </NewThread>
             ) : (
               <Button className="relative w-5 h-5 object-contain">
-                <Image
-                  src={item.icon}
-                  alt={item.name}
-                  fill
-                  className={isActive(item.value) ? "invert" : ""}
-                />
+                <Image src={item.icon} alt={item.name} fill className={isActive(item.value) ? "invert" : ""} />
               </Button>
             )}
           </li>
