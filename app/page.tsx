8e26baeb9f7b5b0e7915db636e593e75128a6537
yuki designs <yukiosdesigns@gmail.com>
2024-05-01 12:52:01 +0300

    Added realtime features

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,110 @@
+'use client'
+import LeftSideBar from "@/components/LeftSideBar";
+import Live from "@/components/Live";
+import Navbar from "@/components/Navbar";
+import RightSideBar from "@/components/RightSideBar";
+import { useEffect, useRef, useState } from "react";
+import {fabric} from "fabric"
+import { handleCanvasMouseDown, handleCanvasMouseUp, handleCanvasObjectModified, handleCanvaseMouseMove, handleResize, initializeFabric, renderCanvas } from "@/lib/canvas";
+import { ActiveElement } from "@/types/type";
+import { useMutation, useStorage } from "@/liveblocks.config";
+import { defaultNavElement } from "@/constants";
+import { handleDelete } from "@/lib/key-events";
 
 export default function Page() {
+  const canvasRef = useRef<HTMLCanvasElement>(null)
+  const fabricRef = useRef<fabric.Canvas | null>(null)
+  const isDrawing = useRef(false)
+  const shapeRef = useRef<fabric.Object | null>(null)
+  const selectedShapeRef = useRef<string | null>(null)
+  const activeObjectRef = useRef<fabric.Object | null>(null)
+
+  const canvasObjects = useStorage((root)=> root.canvasObjects)
+  const syncShapeInStorage = useMutation(({storage}, object) =>{
+    if(!object) return;
+    const {objectId} =object;
+    const shapeData = object.toJSON()
+    shapeData.objectId = objectId
+    const canvasObjects = storage.get('canvasObjects')
+    canvasObjects.set(objectId, shapeData)
+  },[])
+  const [activeElement, setActiveElement] = useState<ActiveElement>({
+    name: '',
+    value:'',
+    icon:''
+  })
+
+  const deleteAllShapes = useMutation(({storage}) =>{
+    const canvasObjects = storage.get('canvasObjects')
+    if(!canvasObjects || canvasObjects.size === 0) return true;
+    for(const [key, value] of canvasObjects.entries()){
+      canvasObjects.delete(key)
+    }
+    return canvasObjects.size === 0
+  },[])
+
+  const deleteShapeFromStorage = useMutation(({storage}, objectId) =>{
+    const canvasObjects = storage.get('canvasObjects')
+    canvasObjects.delete(objectId)
+  },[])
+
+  const handleActiveElement = (elem: ActiveElement) =>{
+    setActiveElement(elem)
+    switch (elem?.value) {
+      case 'reset':
+        deleteAllShapes()
+        fabricRef.current?.clear()
+        setActiveElement(defaultNavElement)
+        break;
+      case 'delete':
+          handleDelete(fabricRef.current as any, deleteShapeFromStorage)
+          setActiveElement(defaultNavElement)
+          break;
+    
+      default:
+        break;
+    }
+    selectedShapeRef.current = elem?.value as string
+  }
+
+  useEffect(()=>{
+    const canvas = initializeFabric({canvasRef, fabricRef})
+    canvas.on("mouse:down", (options)=>{
+      handleCanvasMouseDown({options, canvas, isDrawing, shapeRef, selectedShapeRef})
+    })
+    canvas.on("mouse:move", (options)=>{
+      handleCanvaseMouseMove({options, canvas, isDrawing, shapeRef, selectedShapeRef, syncShapeInStorage})
+    })
+    canvas.on("mouse:up", (options)=>{
+      handleCanvasMouseUp({canvas, isDrawing, shapeRef, selectedShapeRef, syncShapeInStorage, setActiveElement, activeObjectRef})
+    })
+    canvas.on('object:modified', (options)=>{
+      handleCanvasObjectModified({
+        options, syncShapeInStorage
+      })
+    })
+    window.addEventListener("resize", ()=>{
+      handleResize({fabricRef})
+    })
+    return ()=>{
+      canvas.dispose()
+    }
+  }, [])
+
+  useEffect(()=>{
+    renderCanvas({
+      fabricRef, canvasObjects, activeObjectRef
+    })
+  },[canvasObjects])
+
   return (   
-    <div className="h-[100vh] w-full flex justify-center items-center text-center">
-      <h1 className="text-2xl text-white">Live Yuki clone</h1>
-      </div>
+    <main className="h-screen overflow-hidden">
+      <Navbar activeElement={activeElement} handleActiveElement={handleActiveElement} />
+      <section className="flex h-full flex-row">
+        <LeftSideBar />
+          <Live canvasRef={canvasRef} />
+        <RightSideBar />
+      </section>
+    </main>
   );
 }
