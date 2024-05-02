c2855255b1683b606b80985de6533e3918500f8e
yuki designs <yukiosdesigns@gmail.com>
2024-05-02 12:03:45 +0300

    Added Text settings

--- components/RightSideBar.tsx
+++ components/RightSideBar.tsx
@@ -20,8 +20,10 @@ const RightSideBar = ({elementAttributes, setElementAttributes, fabricRef, activ
     <section className='flex flex-col border-t border-primary-grey-200 bg-primary-black text-primary-grey-300 min-2-[227px] sticky right-0 h-full max-sm:hidden select-none'>
         <h3 className='px-5 pt-4 text-xs uppercase'>Design</h3>
         <span className='text-xs text-primary-grey-300 mt-3 px-5 border-b border-primary-grey-200 pb-4 '>Make changes to canvas</span>
-        <Dimensions width={elementAttributes.width} height={elementAttributes.height} handleInputChange={handleInputChange} isEditingRef={isEditingRef} />
-        <Text />
+        <Dimensions width={elementAttributes.width}
+         height={elementAttributes.height} handleInputChange={handleInputChange} isEditingRef={isEditingRef} />
+        <Text fontFamily={elementAttributes.fontFamily} 
+        fontSize={elementAttributes.fontSize} fontWeight={elementAttributes.fontWeight} handleInputChange={handleInputChange} />
         <Color />
         <Color />
         <Export />
